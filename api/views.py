2baddf25ac47eebf9de5e1789f346ad222290531
makenzielarsen <makenzie97larsen@gmail.com>
2018-04-23 21:55:14 -0600

    Serializing the entire json

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from api.models import Conversion
+from django.core import serializers
+from django.shortcuts import HttpResponse
 
-# Create your views here.
+
+def convert(request):
+    conversion = Conversion()
+    conversion.units = "lbs"
+    conversion.value = 2.2
+    data = serializers.serialize('json', [conversion])
+    return HttpResponse(data, content_type="application/json")
